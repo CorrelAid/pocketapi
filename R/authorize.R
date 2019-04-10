01e4ea6a211d2f9cb4c2e36fe55be88cc3749b09
Dorian <d.le-jeune@gmx.de>
2019-04-10 13:47:19 +0200

    edit

--- R/authorize.R
+++ R/authorize.R
@@ -8,40 +8,31 @@ library(usethis)
 
 usethis::edit_r_environ()
 
-consumer_key <- Sys.getenv("POCKET_CONSUMER_KEY")
-consumer_key
-REQUEST_URL <- "https://getpocket.com/v3/oauth/request"
-AUTH_URL <- "https://getpocket.com/v3/oauth/authorize"
-
-request_token_url <- httr::parse_url(REQUEST_URL)
-res <- httr::POST(request_token_url, body = list(consumer_key = consumer_key, redirect_uri = "https://google.com"))
-request_token <- httr::content(res)$code
-
-paste0("https://getpocket.com/auth/authorize?request_token=", request_token, "&redirect_uri=https://www.google.com")
-
-authorize_url <- httr::parse_url(AUTH_URL)
-res <- httr::POST(authorize_url, body = list(consumer_key = consumer_key, code = request_token))
-res
-httr::content(res)
-access_token <- httr::content(res)$access_token
-access_token
+# consumer_key <- Sys.getenv("POCKET_CONSUMER_KEY")
+# consumer_key
+# REQUEST_URL <- "https://getpocket.com/v3/oauth/request"
+# AUTH_URL <- "https://getpocket.com/v3/oauth/authorize"
+#
+# request_token_url <- httr::parse_url(REQUEST_URL)
+# res <- httr::POST(request_token_url, body = list(consumer_key = consumer_key, redirect_uri = "https://google.com"))
+# request_token <- httr::content(res)$code
+#
+# paste0("https://getpocket.com/auth/authorize?request_token=", request_token, "&redirect_uri=https://www.google.com")
+#
+# authorize_url <- httr::parse_url(AUTH_URL)
+# res <- httr::POST(authorize_url, body = list(consumer_key = consumer_key, code = request_token))
+# res
+# httr::content(res)
+# access_token <- httr::content(res)$access_token
+# access_token
 
 get_url <- httr::parse_url("https://getpocket.com/v3/get")
 res <- httr::POST(get_url, body = list(consumer_key = Sys.getenv("POCKET_CONSUMER_KEY"), access_token = Sys.getenv("POCKET_ACCESS_TOKEN")))
 httr::content(res)
 
 # Extracting stuff
-# URL (https://www.reddit.com/r/AskReddit/comments/b9q1zj/what_sounds_like_fiction_but_is_actually_a_real/)
+# Getting an overview
 
-pocket_urls <- httr::content(res)$list$`2548580874`$resolved_url
-pocket_urls
+output <- httr::content(res)$list
 
-# Title
 
-pocket_titles <- httr::content(res)$list$`2548580874`$resolved_title
-pocket_titles
-
-# Upvotes (Excerpt?)
-
-upvotes <- httr::content(res)$list$`2548580874`$excerpt
-upvotes
