ab13d25cd9271d494231c9b0e66eaa483bd5e84f
Unknown <sarah.jukna@outlook.de>
2019-06-20 20:52:58 +0200

    pocket_add

--- R/pocket_add.R
+++ R/pocket_add.R
@@ -25,8 +25,8 @@ pocket_add <- function(consumer.key=NULL, access.token=NULL, add.url=NULL) {
   if (res$status_code==400) stop ("Please check the URL of the item you want to add.")
   if (res$status_code==403) stop ("A valid consumer key for Pocket must be provided.")
   if (res$status_code==401) stop ("A valid request token for Pocket must be provided.")
-  if (res$status_code==200) cat ("You added an item to your pocket list.")
-  #if (res$status_code==200) cat( paste("You added", res$request$fields$url, "to your pocket list."))
+  if (res$status_code==200) message ("You added an item to your pocket list.")
+  #if (res$status_code==200) message( paste("You added", res$request$fields$url, "to your pocket list."))
   
 }
  
