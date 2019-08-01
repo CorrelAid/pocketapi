5c860d09f440ec01079b8d3eb6a316e631984418
Friedrike Preu <friedrike.preu@gmail.com>
2019-08-01 18:25:23 +0200

    bug fix and more tests

--- tests/testthat/test_test.R
+++ tests/testthat/test_test.R
@@ -1,5 +1,19 @@
 library(pocketapi)
+POCKET_TEST_CONSUMER_KEY <- Sys.getenv("POCKET_TEST_CONSUMER_KEY")
+POCKET_TEST_ACCESS_TOKEN <- Sys.getenv("POCKET_TEST_ACCESS_TOKEN")
 
 test_that("Consumer Key invalid error occurs", {
-  expect_error(pocket_get(,access_token=NULL, add_item=c(), raw=FALSE), "Argument 'consumer_key' is missing. A valid consumer key for Pocket must be provided. See 'https://getpocket.com/developer/docs/authentication' on how to obtain one."))
+  expect_error(
+      pocket_get(access_token=NULL, add_item=c(), raw=FALSE),
+    regexp = "Argument 'consumer_key' is missing."
+  )
+})
+
+test_that("return value is data frame", {
+  return_value <- pocket_get(
+    consumer_key = POCKET_TEST_CONSUMER_KEY,
+    access_token = POCKET_TEST_ACCESS_TOKEN
+    )
+  expect_s3_class(return_value, "data.frame")
+  expect_gt(nrow(return_value), 0)
 })
