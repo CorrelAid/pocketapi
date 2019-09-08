3e88ee2fb0b4f3500499c4f4b5268faaa0e24f1f
Christoph Dworschak <dworschak@posteo.de>
2019-09-08 19:41:46 -0400

    1. missing() implemented 2. Request messages added for user to see whether request was successful. 3. all_items argument added, so that not all items need to be typed if user wants all. Compatible with existing 'raw' 4. "Differing number of rows" error resolved (flexible entry assignment when creating the data frame). Only item "domain_metadata" cannot be added right now since the item names there are shit.

--- R/pocket_get.R
+++ R/pocket_get.R
@@ -3,13 +3,15 @@
 #' @param consumer_key character string. Your Pocket consumer key. See https://getpocket.com/developer/docs/authentication.
 #' @param access_token character string. Your Pocket request token. See https://getpocket.com/developer/docs/authentication.
 #' @param add_item character vector. Specify a vector listing additional items to be retrieved from Pocket. Baseline: item ID, URL, and item title.
+#' @param all_items logical. If set to TRUE, all possible items are returned. This is equivalent to manually defining all items in 'add_item'.
 #' @param raw logical. If set to TRUE, pocket data is returned as part of a list that additionally contains all raw information and possible items.
 #' @return dataframe or list.
 #' @export
 #'
-pocket_get <- function(consumer_key = NULL,
-           access_token = NULL,
+pocket_get <- function(consumer_key,
+           access_token,
            add_item = c(),
+           all_items = FALSE,
            raw = FALSE) {
 
 
@@ -26,16 +28,38 @@ pocket_get <- function(consumer_key = NULL,
     res <-
       httr::POST(get_url,
                  body = list(consumer_key = consumer_key, access_token = access_token))
+
+    if(res$status_code==200){print("Your request was successful.")}
+    if(res$status_code==400){print("Invalid request syntax, please contact the package maintainers.")}
+    if(res$status_code==401){print("Authentication problem.")}
+    if(res$status_code==403){print("Authentication successful, but access denied due to lack of permission or rate limiting.")}
+    if(res$status_code==503){print("Pocket's sync server is down for scheduled maintenance. Try again later.")}
+
     output <- httr::content(res)$list
     output.categories <- unique(unlist(lapply(output, names)))
 
     # Generate data set from Pocket output
-    to.be.retrieved <-
-      c(c("item_id", "given_url", "given_title"), add_item)
-    df <-
-      data.frame(lapply(to.be.retrieved, function(item)
-        unlist(lapply(output, function(x)
-          x[[item]]))))
+    if(all_items==FALSE){
+      to.be.retrieved <- c(c("item_id", "given_url", "given_title"), add_item)
+    }else{
+      to.be.retrieved <- c(output.categories[-c(21:24)])
+      to.be.retrieved <- c(output.categories)
+    }
+
+    data <- lapply(to.be.retrieved, function(item)
+                      unlist(lapply(output, function(x)
+                      x[[item]]))) # get the requested item data in list format
+
+    # Retrieve one entry at a time to account for differing lengths (some entries have more items covered than others)
+    mat <- sapply(data, '[', data[[1]][1])
+    if(length(data[[1]]) > 1){
+      for(i in 2:length(data[[1]])){
+      id <- data[[1]][i]
+      mat <- rbind(mat, sapply(data, '[', id) )
+      }
+    }
+
+    df <- data.frame(mat)
     colnames(df) <- to.be.retrieved
     rownames(df) <- 1:nrow(df)
 
