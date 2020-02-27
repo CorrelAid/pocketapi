96bfe761f38f8c2306a5e1e93e2006ff528bb243
Frie <fr1e@pm.me>
2020-02-27 18:02:10 +0100

    add some tests

--- tests/testthat/test_pocket_get.R
+++ tests/testthat/test_pocket_get.R
@@ -8,6 +8,24 @@ test_that("invalid access token causes error", {
                regexp = "401 Unauthorized: A valid access token")
 })
 
+
+test_that("invalid tag value causes error", {
+  expect_error(pocket_get(access_token = POCKET_TEST_ACCESS_TOKEN, consumer_key = POCKET_TEST_CONSUMER_KEY, tag = c("more", "than", "one")),
+               regexp = "The tag argument can only be a character string.")
+})
+
+test_that("invalid favorite value causes error", {
+  expect_error(pocket_get(access_token = POCKET_TEST_ACCESS_TOKEN, consumer_key = POCKET_TEST_CONSUMER_KEY,
+                          favorite = "stringisnotvalid"),
+               regexp = "The favorite argument can only be")
+})
+
+test_that("invalid item_type value causes error", {
+  expect_error(pocket_get(access_token = POCKET_TEST_ACCESS_TOKEN, consumer_key = POCKET_TEST_CONSUMER_KEY,
+                          item_type = "typenotexist"),
+               regexp = "The item_type argument can only be")
+})
+
 test_that("return value is data frame", {
   return_value <- pocket_get(consumer_key = POCKET_TEST_CONSUMER_KEY, access_token = POCKET_TEST_ACCESS_TOKEN)
   expect_s3_class(return_value, "data.frame")
