b7e9ac8565bafa94827cd5fd05377f0f65c9353e
Frie <fr1e@pm.me>
2020-03-14 14:35:19 +0100

    fix rmd check

--- tests/testthat/test_pocket_archive.R
+++ tests/testthat/test_pocket_archive.R
@@ -4,11 +4,12 @@ context("pocket_archive")
 with_mock_api({
     test_that("success generates message", {
         time_stub <- "2020-03-14 12:51:02 CET"
-        # depth = 3 because gen_action_ is called further down
-        mockery::stub(gen_action_, "Sys.time", time_stub, depth = 2)
-        expect_message(
-            pocket_archive(item_ids = c("foobarid"), consumer_key = "fakekey", access_token = "faketoken"),
-            regexp = "Action was successful"
+        with_mock(
+            Sys.time = function() time_stub,
+            expect_message(
+                pocket_archive(item_ids = c("foobarid"), consumer_key = "fakekey", access_token = "faketoken"),
+                regexp = "Action was successful"
+            )
         )
     })
 })
@@ -19,11 +20,12 @@ with_mock_api({
 with_mock_api({
     test_that("one success, one error", {
         time_stub <- "2020-03-14 12:51:02 CET"
-        # depth = 3 because gen_action_ is called further down
-        mockery::stub(gen_action_, "base::Sys.time", time_stub, depth = 2)
-        expect_warning(
-            pocket_archive(item_ids = c("foo", "bar"), consumer_key = "fakekey", access_token = "faketoken"),
-            regexp = "Action on bar failed with error: some error occurred"
+        with_mock(
+            Sys.time = function() time_stub,
+            expect_warning(
+                pocket_archive(item_ids = c("foo", "bar"), consumer_key = "fakekey", access_token = "faketoken"),
+                regexp = "Action on bar failed with error: some error occurred"
+            )
         )
     })
 })
