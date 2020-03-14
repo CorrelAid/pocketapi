67658958d5b92df09351a98d8b41a20ade115dff
Frie <fr1e@pm.me>
2020-03-14 14:41:29 +0100

    two successes

--- tests/testthat/test_pocket_delete.R
+++ tests/testthat/test_pocket_delete.R
@@ -8,13 +8,26 @@ with_mock_api({
             Sys.time = function() time_stub,
             expect_message(
                 pocket_delete(item_ids = c("foobarid"), consumer_key = "fakekey", access_token = "faketoken"),
-                regexp = "Action was successful"
+                regexp = "Action was successful for the items: foobarid"
             )
         )
     })
 })
 
 
+# send-e0dfab-POST.json
+with_mock_api({
+    test_that("two successes", {
+        time_stub <- "2020-03-14 12:51:02 CET"
+        with_mock(
+            Sys.time = function() time_stub,
+            expect_message(
+                pocket_delete(item_ids = c("faz", "bar"), consumer_key = "fakekey", access_token = "faketoken"),
+                regexp = "Action was successful for the items: faz, bar"
+            )
+        )
+    })
+})
 
 # send-11e8e0-POST.json
 with_mock_api({
