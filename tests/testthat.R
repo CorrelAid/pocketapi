5351c6f374715b65952e38cb58ef6fde51cd32d7
Frie <fr1e@pm.me>
2020-03-14 12:41:40 +0100

    add some more tests, add httptest

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,8 @@
 library(testthat)
 library(pocketapi)
+library(httptest)
 
 POCKET_TEST_CONSUMER_KEY <- Sys.getenv("POCKET_TEST_CONSUMER_KEY")
 POCKET_TEST_ACCESS_TOKEN <- Sys.getenv("POCKET_TEST_ACCESS_TOKEN")
 test_check("pocketapi")
+
--- tests/testthat/test_pocket_get.R
+++ tests/testthat/test_pocket_get.R
@@ -3,31 +3,61 @@ context("pocket_get")
 POCKET_TEST_CONSUMER_KEY <- Sys.getenv("POCKET_TEST_CONSUMER_KEY")
 POCKET_TEST_ACCESS_TOKEN <- Sys.getenv("POCKET_TEST_ACCESS_TOKEN")
 
-test_that("invalid access token causes error", {
-  expect_error(pocket_get(access_token = "dsffköwejrl", consumer_key = POCKET_TEST_CONSUMER_KEY),
-               regexp = "\n401 Unauthorized: A valid access token")
+
+test_that("empty consumer key causes error", {
+  expect_error(pocket_get(
+    access_token = POCKET_TEST_ACCESS_TOKEN, consumer_key = "",
+    favorite = "stringisnotvalid"
+  ),
+  regexp = "^POCKET_CONSUMER_KEY does not exist as environment variable. "
+  )
 })
 
+test_that("empty access token causes error", {
+  expect_error(pocket_get(
+    access_token = "", consumer_key = POCKET_TEST_CONSUMER_KEY,
+    favorite = "stringisnotvalid"
+  ),
+  regexp = "^POCKET_ACCESS_TOKEN does not exist as environment variable"
+  )
+})
 
 test_that("invalid tag value causes error", {
   expect_error(pocket_get(access_token = POCKET_TEST_ACCESS_TOKEN, consumer_key = POCKET_TEST_CONSUMER_KEY, tag = c("more", "than", "one")),
-               regexp = "^The tag argument can only be a character string.")
-})
+    regexp = "^The tag argument can only be a character string."
+  )
 
-test_that("invalid favorite value causes error", {
-  expect_error(pocket_get(access_token = POCKET_TEST_ACCESS_TOKEN, consumer_key = POCKET_TEST_CONSUMER_KEY,
-                          favorite = "stringisnotvalid"),
-               regexp = "^The favorite argument can only be")
+  test_that("invalid favorite value causes error", {
+    expect_error(pocket_get(
+      access_token = POCKET_TEST_ACCESS_TOKEN, consumer_key = POCKET_TEST_CONSUMER_KEY,
+      favorite = "stringisnotvalid"
+    ),
+    regexp = "^The favorite argument can only be"
+    )
+  })
 })
 
 test_that("invalid item_type value causes error", {
-  expect_error(pocket_get(access_token = POCKET_TEST_ACCESS_TOKEN, consumer_key = POCKET_TEST_CONSUMER_KEY,
-                          item_type = "typenotexist"),
-               regexp = "^The item_type argument can only be")
+  expect_error(pocket_get(
+    access_token = POCKET_TEST_ACCESS_TOKEN, consumer_key = POCKET_TEST_CONSUMER_KEY,
+    item_type = "typenotexist"
+  ),
+  regexp = "^The item_type argument can only be"
+  )
 })
 
-test_that("return value is data frame", {
-  return_value <- pocket_get(consumer_key = POCKET_TEST_CONSUMER_KEY, access_token = POCKET_TEST_ACCESS_TOKEN)
-  expect_s3_class(return_value, "data.frame")
-  expect_gt(nrow(return_value), 0)
+httptest::with_mock_api({
+  test_that("invalid access token causes error", {
+    expect_error(pocket_get(access_token = "dsffköwejrl", consumer_key = POCKET_TEST_CONSUMER_KEY),
+      regexp = "\n401 Unauthorized: A valid access token"
+    )
+  })
+})
+
+httptest::with_mock_api({
+  test_that("return value is data frame", {
+    return_value <- pocket_get(consumer_key = POCKET_TEST_CONSUMER_KEY, access_token = POCKET_TEST_ACCESS_TOKEN)
+    expect_s3_class(return_value, "data.frame")
+    expect_gt(nrow(return_value), 0)
+  })
 })
