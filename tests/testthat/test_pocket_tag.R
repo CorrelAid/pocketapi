2e9aa82a4449c46669d26c5c9149834c918373d4
Frie <fr1e@pm.me>
2020-03-15 21:39:38 +0100

    fix test and documentation

--- tests/testthat/test_pocket_tag.R
+++ tests/testthat/test_pocket_tag.R
@@ -2,8 +2,7 @@ context("pocket_tag")
 
 
 test_that("pocket_tag throws error for invalid action type", {
-    expect_error(pocket_tag("tag_remove"), # it's tags_remove
+    expect_error(pocket_tag("tag_remove", consumer_key = "myconsumerkey", access_token = "myaccesstoken"), # it's tags_remove
         regexp = "Tag actions can be only be: 'tags_add', 'tags_remove', 'tags_replace', 'tags_clear', 'tag_rename', or 'tag_delete'."
     )
 })
-
