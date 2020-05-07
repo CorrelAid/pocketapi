8d705f477ab41a3b465650b265ff3e93ed22087c
Frie <fr1e@pm.me>
2020-05-07 11:17:49 +0200

    more tests for pocket_tag, refactoring

--- tests/testthat/getpocket.com/v3/oauth/authorize-1384bf-POST.R
+++ tests/testthat/getpocket.com/v3/oauth/authorize-1384bf-POST.R
@@ -1,48 +1,25 @@
-structure(list(
-    url = "https://getpocket.com/v3/oauth/authorize",
-    status_code = 400L, headers = structure(list(
-        date = "Sun, 15 Mar 2020 11:06:13 GMT",
-        `content-type` = "text/html; charset=UTF-8", `content-length` = "15",
-        server = "Apache/2.4.25 (Debian)", `x-frame-options` = "SAMEORIGIN",
-        `x-error` = "Code not found.", `x-error-code` = "185",
-        status = "400 Bad Request", `cache-control` = "private",
-        `x-source` = "Pocket", `set-cookie` = "REDACTED", p3p = "policyref=\"/w3c/p3p.xml\", CP=\"ALL CURa ADMa DEVa OUR IND UNI COM NAV INT STA PRE\""
-    ), class = c(
-        "insensitive",
-        "list"
-    )), all_headers = list(list(
-        status = 400L, version = "HTTP/2",
-        headers = structure(list(
-            date = "Sun, 15 Mar 2020 11:06:13 GMT",
-            `content-type` = "text/html; charset=UTF-8", `content-length` = "15",
-            server = "Apache/2.4.25 (Debian)", `x-frame-options` = "SAMEORIGIN",
-            `x-error` = "Code not found.", `x-error-code` = "185",
-            status = "400 Bad Request", `cache-control` = "private",
-            `x-source` = "Pocket", `set-cookie` = "REDACTED",
-            p3p = "policyref=\"/w3c/p3p.xml\", CP=\"ALL CURa ADMa DEVa OUR IND UNI COM NAV INT STA PRE\""
-        ), class = c(
-            "insensitive",
-            "list"
-        ))
-    )), cookies = structure(list(domain = c(
-        "#HttpOnly_getpocket.com",
-        "#HttpOnly_getpocket.com"
-    ), flag = c(FALSE, FALSE), path = c(
-        "/",
-        "/"
-    ), secure = c(FALSE, FALSE), expiration = structure(c(
-        Inf,
-        1584273973
-    ), class = c("POSIXct", "POSIXt")), name = c(
-        "PHPSESSID",
-        "AUTH_BEARER_default"
-    ), value = c("REDACTED", "REDACTED")), row.names = c(
-        NA,
-        -2L
-    ), class = "data.frame"), content = charToRaw("400 Bad Request"),
-    date = structure(1584270373, class = c("POSIXct", "POSIXt"), tzone = "GMT"), times = c(
-        redirect = 0, namelookup = 7e-05,
-        connect = 7.2e-05, pretransfer = 0.000202, starttransfer = 0.00022,
-        total = 0.164206
-    )
-), class = "response")
+structure(list(url = "https://getpocket.com/v3/oauth/authorize", 
+    status_code = 400L, headers = structure(list(date = "Sun, 15 Mar 2020 11:06:13 GMT", 
+        `content-type` = "text/html; charset=UTF-8", `content-length` = "15", 
+        server = "Apache/2.4.25 (Debian)", `x-frame-options` = "SAMEORIGIN", 
+        `x-error` = "Code not found.", `x-error-code` = "185", 
+        status = "400 Bad Request", `cache-control` = "private", 
+        `x-source` = "Pocket", `set-cookie` = "REDACTED", p3p = "policyref=\"/w3c/p3p.xml\", CP=\"ALL CURa ADMa DEVa OUR IND UNI COM NAV INT STA PRE\""), class = c("insensitive", 
+    "list")), all_headers = list(list(status = 400L, version = "HTTP/2", 
+        headers = structure(list(date = "Sun, 15 Mar 2020 11:06:13 GMT", 
+            `content-type` = "text/html; charset=UTF-8", `content-length` = "15", 
+            server = "Apache/2.4.25 (Debian)", `x-frame-options` = "SAMEORIGIN", 
+            `x-error` = "Code not found.", `x-error-code` = "185", 
+            status = "400 Bad Request", `cache-control` = "private", 
+            `x-source` = "Pocket", `set-cookie` = "REDACTED", 
+            p3p = "policyref=\"/w3c/p3p.xml\", CP=\"ALL CURa ADMa DEVa OUR IND UNI COM NAV INT STA PRE\""), class = c("insensitive", 
+        "list")))), cookies = structure(list(domain = c("#HttpOnly_getpocket.com", 
+    "#HttpOnly_getpocket.com"), flag = c(FALSE, FALSE), path = c("/", 
+    "/"), secure = c(FALSE, FALSE), expiration = structure(c(Inf, 
+    1584273973), class = c("POSIXct", "POSIXt")), name = c("PHPSESSID", 
+    "AUTH_BEARER_default"), value = c("REDACTED", "REDACTED")), row.names = c(NA, 
+    -2L), class = "data.frame"), content = charToRaw("400 Bad Request"), 
+    date = structure(1584270373, class = c("POSIXct", "POSIXt"
+    ), tzone = "GMT"), times = c(redirect = 0, namelookup = 7e-05, 
+    connect = 7.2e-05, pretransfer = 0.000202, starttransfer = 0.00022, 
+    total = 0.164206)), class = "response")
--- tests/testthat/getpocket.com/v3/oauth/request-b9df5b-POST.R
+++ tests/testthat/getpocket.com/v3/oauth/request-b9df5b-POST.R
@@ -1,48 +1,25 @@
-structure(list(
-    url = "https://getpocket.com/v3/oauth/request",
-    status_code = 403L, headers = structure(list(
-        date = "Sun, 15 Mar 2020 11:21:17 GMT",
-        `content-type` = "text/html; charset=UTF-8", `content-length` = "13",
-        server = "Apache/2.4.25 (Debian)", `x-frame-options` = "SAMEORIGIN",
-        `x-error` = "Invalid consumer key.", `x-error-code` = "152",
-        status = "403 Forbidden", `cache-control` = "private",
-        `x-source` = "Pocket", `set-cookie` = "REDACTED", p3p = "policyref=\"/w3c/p3p.xml\", CP=\"ALL CURa ADMa DEVa OUR IND UNI COM NAV INT STA PRE\""
-    ), class = c(
-        "insensitive",
-        "list"
-    )), all_headers = list(list(
-        status = 403L, version = "HTTP/2",
-        headers = structure(list(
-            date = "Sun, 15 Mar 2020 11:21:17 GMT",
-            `content-type` = "text/html; charset=UTF-8", `content-length` = "13",
-            server = "Apache/2.4.25 (Debian)", `x-frame-options` = "SAMEORIGIN",
-            `x-error` = "Invalid consumer key.", `x-error-code` = "152",
-            status = "403 Forbidden", `cache-control` = "private",
-            `x-source` = "Pocket", `set-cookie` = "REDACTED",
-            p3p = "policyref=\"/w3c/p3p.xml\", CP=\"ALL CURa ADMa DEVa OUR IND UNI COM NAV INT STA PRE\""
-        ), class = c(
-            "insensitive",
-            "list"
-        ))
-    )), cookies = structure(list(domain = c(
-        "#HttpOnly_getpocket.com",
-        "#HttpOnly_getpocket.com"
-    ), flag = c(FALSE, FALSE), path = c(
-        "/",
-        "/"
-    ), secure = c(FALSE, FALSE), expiration = structure(c(
-        Inf,
-        1584274877
-    ), class = c("POSIXct", "POSIXt")), name = c(
-        "PHPSESSID",
-        "AUTH_BEARER_default"
-    ), value = c("REDACTED", "REDACTED")), row.names = c(
-        NA,
-        -2L
-    ), class = "data.frame"), content = charToRaw("403 Forbidden"),
-    date = structure(1584271277, class = c("POSIXct", "POSIXt"), tzone = "GMT"), times = c(
-        redirect = 0, namelookup = 5.8e-05,
-        connect = 6e-05, pretransfer = 0.000164, starttransfer = 0.000184,
-        total = 0.168642
-    )
-), class = "response")
+structure(list(url = "https://getpocket.com/v3/oauth/request", 
+    status_code = 403L, headers = structure(list(date = "Sun, 15 Mar 2020 11:21:17 GMT", 
+        `content-type` = "text/html; charset=UTF-8", `content-length` = "13", 
+        server = "Apache/2.4.25 (Debian)", `x-frame-options` = "SAMEORIGIN", 
+        `x-error` = "Invalid consumer key.", `x-error-code` = "152", 
+        status = "403 Forbidden", `cache-control` = "private", 
+        `x-source` = "Pocket", `set-cookie` = "REDACTED", p3p = "policyref=\"/w3c/p3p.xml\", CP=\"ALL CURa ADMa DEVa OUR IND UNI COM NAV INT STA PRE\""), class = c("insensitive", 
+    "list")), all_headers = list(list(status = 403L, version = "HTTP/2", 
+        headers = structure(list(date = "Sun, 15 Mar 2020 11:21:17 GMT", 
+            `content-type` = "text/html; charset=UTF-8", `content-length` = "13", 
+            server = "Apache/2.4.25 (Debian)", `x-frame-options` = "SAMEORIGIN", 
+            `x-error` = "Invalid consumer key.", `x-error-code` = "152", 
+            status = "403 Forbidden", `cache-control` = "private", 
+            `x-source` = "Pocket", `set-cookie` = "REDACTED", 
+            p3p = "policyref=\"/w3c/p3p.xml\", CP=\"ALL CURa ADMa DEVa OUR IND UNI COM NAV INT STA PRE\""), class = c("insensitive", 
+        "list")))), cookies = structure(list(domain = c("#HttpOnly_getpocket.com", 
+    "#HttpOnly_getpocket.com"), flag = c(FALSE, FALSE), path = c("/", 
+    "/"), secure = c(FALSE, FALSE), expiration = structure(c(Inf, 
+    1584274877), class = c("POSIXct", "POSIXt")), name = c("PHPSESSID", 
+    "AUTH_BEARER_default"), value = c("REDACTED", "REDACTED")), row.names = c(NA, 
+    -2L), class = "data.frame"), content = charToRaw("403 Forbidden"), 
+    date = structure(1584271277, class = c("POSIXct", "POSIXt"
+    ), tzone = "GMT"), times = c(redirect = 0, namelookup = 5.8e-05, 
+    connect = 6e-05, pretransfer = 0.000164, starttransfer = 0.000184, 
+    total = 0.168642)), class = "response")
