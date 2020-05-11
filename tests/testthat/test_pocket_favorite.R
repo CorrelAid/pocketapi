1e73a135fe8f5d65e1f12f1d0ee0da1bc5c397f1
Max Alletsee <max.alletsee@gmail.com>
2020-05-11 07:57:39 +0200

    Adding unit testing for pocket_favorite and pocket_unfavorite

---
+++ tests/testthat/test_pocket_favorite.R
@@ -0,0 +1,30 @@
+context("pocket_favorite")
+
+
+test_that("missing consumer key causes error", {
+    expect_error(
+        pocket_favorite(item_ids = c("foobarid"), consumer_key = "", access_token = "faketoken"),
+        regexp = "^POCKET_CONSUMER_KEY does not exist as environment variable."
+    )
+})
+
+test_that("missing access token causes error", {
+    expect_error(
+        pocket_favorite(item_ids = c("foobarid"), consumer_key = "fakekey", access_token = ""),
+        regexp = "^POCKET_ACCESS_TOKEN does not exist as environment variable."
+    )
+})
+
+# send-ea652b-POST.json
+with_mock_api({
+    test_that("pocket_favorite - success generates message", {
+        time_stub <- "2020-03-14 12:51:02 CET"
+        with_mock(
+            Sys.time = function() time_stub,
+            expect_message(
+                pocket_favorite(item_ids = c("foobarid"), consumer_key = "fakekey", access_token = "faketoken"),
+                regexp = "Action was successful"
+            )
+        )
+    })
+})
